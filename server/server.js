d1d66ad457d8be6c2013eed76a2c5a80c1e28ca7
KarolisRZN <qewmnbp@gmail.com>
2025-05-06 13:26:07 +0300

    added correct structure

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const toursRoutes = require('./routes/tours');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json()); 
+
+app.use('/api/tours', toursRoutes); // routes for tours
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
