d82e448f6756b796e8f9c8685594491c68e1870e
KarolisRZN <qewmnbp@gmail.com>
2025-05-09 13:42:57 +0300

    fixed image upload

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 const toursRoutes = require("./routes/toursRoutes");
 const setUserRoutes = require("./routes/userRoutes");
 const tourDatesRoutes = require("./routes/tourDatesRoutes");
@@ -18,7 +19,8 @@ const PORT = config.PORT || process.env.PORT || 5000;
 app.use(cors());
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(express.json()); // Middleware for JSON body parsing
+app.use(express.json({ limit: "10mb" })); // Middleware for JSON body parsing
+app.use(bodyParser.json({ limit: "10mb" })); // Middleware for JSON body parsing
 
 app.use("/api/tours", toursRoutes);
 app.use("/api/tour-dates", tourDatesRoutes);
